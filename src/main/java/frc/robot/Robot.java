1e75997b6cab208587b51431ec73be8a0628a0ea
bruingineer <joshcamarena@ucla.edu>
2019-11-05 20:35:23 -0800

    4415 subsystem module start

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.frcteam2910.common.robot.drivers.Mk2SwerveModule;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
